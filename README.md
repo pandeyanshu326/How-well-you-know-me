53475e1356153cdc71a227c1ebc4cd9dfc9cd541
Anshu Pandey <pandeyanshu326@gmail.com>
2021-09-13 21:56:52 +0530

    how much do you know me

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# How well you know me
- This is CLI based game which has few questions related to me and answering it correctly will give points. 
+# How well do you know me
+ This is a CLI based game which has few questions related to me and answering it correctly will give points. 
+ It's a fun game and language used is Javascript.
---
+++ index.js
@@ -0,0 +1,48 @@
+var readlineSync = require("readline-sync");
+let score = 0;
+let userName = readlineSync.question("What's your name? ");
+console.log("Welcome " + userName + " to Do you know Anshu?");
+
+//Play Function
+function play(question, answer){
+  let userAnswer = readlineSync.question(question);
+
+  if(userAnswer == answer){
+    console.log("Correct");
+    score = score+1;
+  }
+  else{
+    console.log("Wrong");
+  }
+
+  console.log("current score : ", score);
+}
+
+//Array of Objects
+let questions = [
+  {
+  question: "Where does Anshu live? ",
+  answer: "jammu"
+  },
+  {
+  question: "What's her favourite color? ",
+  answer: "cyan"
+  }, 
+  {
+  question: "How many siblings does she have? ",
+  answer: "2"
+  },
+  {
+  question: "What's her birthday date? ",
+  answer: "20"
+  },
+
+];
+
+//Loop
+for(i=0; i<questions.length; i++){
+  var recenttQuestion = questions[i];
+  play(recentQuestion.question, recentQuestion.answer)
+}
+
+console.log("Great! Your final score is: ", score)
